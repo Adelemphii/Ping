8b2680fd966b985799407b78da9d95acda0492af
Adelemphii <demilitioowner@gmail.com>
2021-12-29 21:12:08 -0600

    Initial commit

---
+++ src/main/java/me/adelemphii/ping/Ping.java
@@ -0,0 +1,17 @@
+package me.adelemphii.ping;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ping extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
